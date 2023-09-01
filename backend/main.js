893ec3b2c59f1736573900461aa4a25dc065c871
Abhijit Paul <bsse1201@iit.du.ac.bd>
2023-09-01 10:23:24 +0600

    done with main part minimax

--- backend/main.js
+++ backend/main.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const minimax = require("./algorithm/Minimax.js")
 
 const app = express();
 app.use(cors());
@@ -9,6 +10,23 @@ const PORT = 3000;
 
 app.get("/", (req, res)=>{
     res.status(200).send({"message":"Server Running"});
+});
+
+app.post("/ai/solve", (req, res)=>{
+    let state = req.body.state;
+    let algoResult = minimax.expandAndFindSolution(new minimax.Node(Math.max(), Math.min(),0,state),0);
+
+    let nextMove = null;
+    algoResult.children.forEach((child)=>{
+	if(child.minimaxValue == algoResult.minimaxValue) {
+	    nextMove = child;
+	    child.printNode();
+	    return;
+	}
+    })
+    console.log("Result is: ");
+    nextMove.printNode();
+    res.status(200).send({"state": nextMove.state});
 });
 
 app.listen(PORT, (data)=>{
